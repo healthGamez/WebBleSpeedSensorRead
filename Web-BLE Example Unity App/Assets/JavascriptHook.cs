9692a971a6c761bc21761ee7311e3c4166f85493
Nathan Rona <healthgamez57@gmail.com>
2021-02-24 17:37:24 +0200

    Added Unity App

---
+++ Web-BLE Example Unity App/Assets/JavascriptHook.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JavascriptHook : MonoBehaviour
+{
+    private Text cumulativeRoundsValue;
+    private Text lastTimeStampValue;
+    private Text speedValue;
+
+    private void Start()
+    {
+        cumulativeRoundsValue = GameObject.Find("CumulativeRoundsValue").GetComponent<UnityEngine.UI.Text>();
+        lastTimeStampValue= GameObject.Find("LastTimeStampValue").GetComponent<UnityEngine.UI.Text>();
+        lastTimeStampValue = GameObject.Find("LastTimeStampValue").GetComponent<UnityEngine.UI.Text>();
+        speedValue = GameObject.Find("SpeedValue").GetComponent<UnityEngine.UI.Text>();
+    }
+
+    private void Update()
+    {
+        
+    }
+}
